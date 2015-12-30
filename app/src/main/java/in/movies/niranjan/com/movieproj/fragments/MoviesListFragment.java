7133dff73060b1dc57620064f7f0fd27a9b62b60
niranjan1624 <niranjan.ece1@gmail.com>
2015-12-30 15:49:20 +0530

    added menu options

--- app/src/main/java/in/movies/niranjan/com/movieproj/fragments/MoviesListFragment.java
+++ app/src/main/java/in/movies/niranjan/com/movieproj/fragments/MoviesListFragment.java
@@ -3,6 +3,8 @@ package in.movies.niranjan.com.movieproj.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -22,5 +24,10 @@ public class MoviesListFragment extends BaseFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_movie_list, menu);
     }
 }
